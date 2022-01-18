2bd409e5a3c2da344ab9aa85b7b2b3d790a677f3
KuranovIlya <43750944+KuranovIlya@users.noreply.github.com>
2022-01-18 17:15:05 +0300

    Инициализация проекта

---
+++ AmbulanceWebLibrary/WebServer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbulanceWebLibrary
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet]
+        //[WebInvoke(Method = "GET",
+        //   RequestFormat = WebMessageFormat.Json,
+        //   ResponseFormat = WebMessageFormat.Json)]
+        string CheckConnection();
+    }
+
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single, UseSynchronizationContext = false)]
+    public class SimplexAmbulanceService : IService
+    {
+        public string CheckConnection()
+        {
+            return "123";
+        }
+    }
+    public class WebServer
+    {
+        Uri uri;
+        ServiceHost host;
+
+
+        public bool Start()
+        {
+            try
+            {
+                //Инициализация сервиса и открытия порта для запросов
+                uri = new Uri("http://localhost:8100/");
+                host = new ServiceHost(typeof(SimplexAmbulanceService), uri);
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+
+                host.AddServiceEndpoint(typeof(IService), new BasicHttpBinding(), "Soap");
+                ServiceEndpoint endpoint = host.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "Web");
+                endpoint.Behaviors.Add(new WebHttpBehavior());
+
+                host.Open();
+            }
+            catch (Exception exc)
+            {
+            }
+            return true;
+        }
+    }
+}
---
+++ AmbulanceWebTest/Form1.cs
@@ -0,0 +1,24 @@
+﻿using AmbulanceWebLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmbulanceWebTest
+{
+    public partial class Form1 : Form
+    {
+        WebServer webServer;
+        public Form1()
+        {
+            InitializeComponent();
+            webServer = new WebServer();
+            webServer.Start();
+        }
+    }
+}
